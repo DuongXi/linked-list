34807de01cf12b1a72f1c5bb97c586543d9569d7
DuongXi <97423916+DuongXi@users.noreply.github.com>
2023-05-10 20:36:50 +0700

    Create _test.py

---
+++ _test.py
@@ -0,0 +1,22 @@
+from linked_list import *
+'''
+Một số câu lệnh dể thử
+lưu ý có thể chưa cover hết tất cả trường hợp
+'''
+
+
+# list = linkedList(1)
+# list.appendList(2)
+# list.printList()
+# list.addFirst(3)
+# list.printList()
+# list.removeHead()
+# list.printList()
+# list.insert(5,2)
+# list.printList()
+# list.appendList(7)
+# list.appendList(6)
+# list.printList()
+# list.remove(5)
+# list.printList()
+# print(list.length())
